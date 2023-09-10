4a8ad06227e8482819138e18e22ad994f81ed991
Jez20 <Johneleazar.sandi.iics@ust.edu.ph>
2023-09-10 20:19:14 +0800

    Buttons
    Scene Transitions

---
+++ Assets/SceneNavigator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigator : MonoBehaviour
+{
+    private int currentIndex = 0; // Keep track of the current scene index
+    private string[] sceneNames = {
+        "CharacterSelect",
+        "MapSelect",
+        "FlipCoin",
+        "GameProper",
+        "WinBanner",
+        "EvaluateBanner",
+    };
+
+    public void LoadNextScene()
+    {
+        if (currentIndex < sceneNames.Length - 1)
+        {
+            currentIndex++;
+            SceneManager.LoadScene(sceneNames[currentIndex]);
+        }
+    }
+
+    public void LoadPreviousScene()
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+            SceneManager.LoadScene(sceneNames[currentIndex]);
+        }
+    }
+
+    public void LoadHomeScene()
+    {
+        // Replace "Home" with the name of your home scene
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
