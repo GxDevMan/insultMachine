8ca87a7fca92f0ba51d3b401268111402714d862
Feivel Doctora <fvdoctora@gmail.com>
2023-09-16 02:14:30 +0800

    ai connection methods

---
+++ Assets/Code/aiRequester.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+public class aiRequester
+{
+    private string apiUrl = "http://127.0.0.1:8000/cnnsvm/aiJudge/";
+    public delegate void PostRequestCallback(string result);
+    private PostRequestCallback postRequestCallback;
+
+    public IEnumerator SendPostRequest(List<string> textData, PostRequestCallback callback)
+    {
+        postRequestCallback = callback;
+        yield return judgeText(textData);
+    }
+
+    private IEnumerator judgeText(List<string> textsToSend)
+    {
+        string compiledArr = "";
+
+        int size = textsToSend.Count;
+        int index = 0;
+
+        while (index < size)
+        {
+            if (size == 1)
+            {
+                compiledArr += $"\"{textsToSend[index]}\"";
+                index++;
+            }
+            else if(size > 1)
+            {
+                if (index == size-1)
+                {
+                    compiledArr += $"\"{textsToSend[index]}\"";
+                    index++;
+
+                }
+                else
+                {
+                    compiledArr += $"\"{textsToSend[index]}\",";
+                    index++;
+                }
+            }
+        }
+        string jsonData = $"{{ \"text\": [{compiledArr}]}}";
+
+        using (UnityWebRequest request = UnityWebRequest.Post(apiUrl, jsonData))
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.LogError("POST Request Failed: " + request.error);
+            }
+            else
+            {
+                string responseData = request.downloadHandler.text;
+                postRequestCallback?.Invoke(responseData);
+            }
+        }
+    }
+
+    public void RefreshAI()
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(apiUrl);
+        webRequest.SendWebRequest();
+    }
+}
---
+++ Assets/Code/testSceneC.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class testSceneC : MonoBehaviour
+{
+    private aiRequester theJudge;
+    public InputField inputArea;
+    public InputField outputArea;
+    public Button sendText;
+    void Start()
+    {
+        theJudge = new aiRequester();
+        sendText.onClick.AddListener(arbritate);
+    }
+
+    private void arbritate()
+    {
+        char[] delimiters = { ';' };
+
+        string[] items = inputArea.text.Split(delimiters, StringSplitOptions.None);
+        List<string> textsToSend = new List<string>();
+
+        for(int i = 0; i < items.Length; i++)
+        {
+            textsToSend.Add(items[i]);
+        }
+
+        StartCoroutine(theJudge.SendPostRequest(textsToSend, HandlePostRequestResult));
+    }
+
+    private void HandlePostRequestResult(string result)
+    {
+        string resultJudge = "";
+        if (!string.IsNullOrEmpty(result))
+        {
+            VerdictData verdictdata = JsonUtility.FromJson<VerdictData>(result);
+            List<int> judgementNumbers = verdictdata.verdict;
+
+            if (judgementNumbers.Count > 0)
+            {
+                if (judgementNumbers.Count == 1)
+                {
+                    resultJudge += judgementNumbers[0];
+                }
+                else
+                {
+                    int index = 0;
+
+                    while(index < judgementNumbers.Count)
+                    {
+                        if (index == judgementNumbers.Count - 1)
+                        {
+                            resultJudge += judgementNumbers[index];
+                            index++;
+                        }
+                        else
+                        {
+                            resultJudge += judgementNumbers[index] + ",";
+                            index++;
+                        }
+                    }
+                }
+                outputArea.text = resultJudge;
+            }
+            else
+            {
+                outputArea.text = "No Result";
+            }
+        }
+        else
+        {
+            Debug.LogError("No result or request failed.");
+        }
+    }
+    [System.Serializable]
+    public class VerdictData
+    {
+        public List<int> verdict;
+    }
+}
