0afd2a12d14b2f414719765a569cc0115619b24c
Feivel Doctora <fvdoctora@gmail.com>
2023-09-26 15:23:37 +0800

    Implemented Match Manager

--- Assets/Code/MatchManager.cs
+++ Assets/Code/MatchManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class MatchManager : MonoBehaviour
 {
     public static MatchManager instance;
+    public static event HandleResult OnFinishTurn;
+    public delegate void HandleResult(double result);
+
     public playerObj player1 { get; set; }
     public playerObj player2 { get; set; }
     public bool cnnsvmSetting { get; set; }
     public int matchId { get; set; }
-
 
     private SQLliteHandler handleSql;
     private string sqlLoc;
@@ -19,38 +21,16 @@ public class MatchManager : MonoBehaviour
     private aiRequester theJudge;
 
     public double result { get; set; }
-
-
     void Start()
     {
-        handleSql.CreateTable();
+        
         string template = "URI=file:";
         string relativeLoc = "Assets/Code/data/loggedArbitration.db";
         sqlLoc = $"{template}{relativeLoc}";
 
         this.theJudge = new aiRequester();
         this.handleSql = new SQLliteHandler(sqlLoc, relativeLoc);
-
-        player1 = new playerObj("Player 1", 100, 20, 20);
-        player2 = new playerObj("Player 2", 100, 20, 20);
-
-        this.matchId = handleSql.newMatch();
-        player1.playerId = handleSql.newPlayer(player1);
-        player2.playerId = handleSql.newPlayer(player2);
-
-        if (firstPlayer == firstPlayer)
-        {
-            this.currentPlayer = player1;
-        }
-        else
-        {
-            this.currentPlayer = player2;
-        }
-    }
-
-    void Update()
-    {
-        
+        handleSql.CreateTable();
     }
 
     private void Awake()
@@ -66,46 +46,38 @@ public class MatchManager : MonoBehaviour
         }
     }
 
-    private void judging(string text)
+    public void setFirstPlayer(bool firstPlayer)
     {
-        StartCoroutine(ArbitrateAndSwap(text));
-    }
-
-    private IEnumerator ArbitrateAndSwap(string text)
-    {
-        string input = text;
-        yield return StartCoroutine(arbitrate(input));
-
-        if (currentPlayer.playerName == "Player 1")
+        if (firstPlayer)
         {
-            currentPlayer = player2;
-            Debug.Log($"Current Player: {currentPlayer.playerId}");
+            this.currentPlayer = player1;
+            Debug.Log($"first turn is player1: {currentPlayer.playerId}");
         }
         else
         {
-            currentPlayer = player1;
-            Debug.Log($"Current Player: {currentPlayer.playerId}");
+            this.currentPlayer = player2;
+            Debug.Log($"first turn is player2: {currentPlayer.playerId}");
         }
     }
 
+    public void newMatch(playerObj player1, playerObj player2)
+    {
+        this.matchId = handleSql.newMatch();
+        this.player1 = player1;
+        this.player2 = player2;
+        player1.playerId = handleSql.newPlayer(player1);
+        player2.playerId = handleSql.newPlayer(player2);
+    }
+
+    public void judging(string text)
+    {
+        StartCoroutine(arbitrate(text));
+    }
     private IEnumerator arbitrate(string input)
     {
         statementObj newstatementChatFilter = new statementObj(currentPlayer.playerId, matchId, input);
         yield return StartCoroutine(theJudge.SendJudgementPostRequest(newstatementChatFilter, handleJudgeMent));
     }
-
-
-    public void newMatch()
-    {
-        this.matchId = handleSql.newMatch();
-
-        player1 = new playerObj("Player 1", 100, 20, 20);
-        player2 = new playerObj("Player 2", 100, 20, 20);
-
-        player1.playerId = handleSql.newPlayer(player1);
-        player2.playerId = handleSql.newPlayer(player2);
-    }
-
     private void handleJudgeMent(statementObj result)
     {
         handleSql.InsertStatement(result);
@@ -136,6 +108,31 @@ public class MatchManager : MonoBehaviour
             }
 
         }
+        swapPlayers();
+
+        if (cnnsvmSetting)
+        {
+            OnFinishTurn?.Invoke(result.ratingCNNSVM);
+        }
+        else
+        {
+            OnFinishTurn?.Invoke(result.ratingChatFilter);
+        }
+    }
+
+    private void swapPlayers()
+    {
+        if (currentPlayer == player1)
+        {
+            currentPlayer = player2;
+
+        }
+        else
+        {
+            currentPlayer = player1;
+        }
+
+        Debug.Log($"Switching players. Current player ID: {currentPlayer.playerId}");
     }
 
     private void resultJudge(statementObj result)
@@ -154,13 +151,11 @@ public class MatchManager : MonoBehaviour
     {
         if (currentPlayer == player1)
         {
-            player2.health -= (int)(player1.maxDamage * percentDamage);
-            currentPlayer = this.player2;
+            player2.health -= (int)(player1.maxDamage * percentDamage);  
         }
         else
         {
             player1.health -= (int)(player2.maxDamage * percentDamage);
-            currentPlayer = this.player1;
         }
     }
 
@@ -173,7 +168,6 @@ public class MatchManager : MonoBehaviour
             {
                 player2.health = player2.maxHealth;
             }
-            currentPlayer = player1;
         }
         else
         {
@@ -182,7 +176,7 @@ public class MatchManager : MonoBehaviour
             {
                 player1.health = player1.maxHealth;
             }
-            currentPlayer = player2;
         }
     }
+
 }
--- Assets/Code/testSceneC.cs
+++ Assets/Code/testSceneC.cs
@@ -1,159 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-
+using UnityEngine.Events;
 public class testSceneC : MonoBehaviour
 {
     public InputField inputArea;
     public InputField outputArea;
     public Button sendText;
-    private string sqlLoc;
 
-    private aiRequester theJudge;
     private int matchId;
-    public bool cnnsvmSetting { get; set; }
-    private SQLliteHandler handleSql;
-    public playerObj player1 { get; set; }
-    public playerObj player2 { get; set; }
-
-    public playerObj currentPlayer;
-    string firstPlayer;
-
+    MatchManager matchmanager;
     void Start()
     {
-        string template = "URI=file:";
-        string relativeLoc = "Assets/Code/data/loggedArbitration.db";
-        sqlLoc = $"{template}{relativeLoc}";
+        matchmanager = MatchManager.instance;
+        playerObj player1 = new playerObj("player1",50,10,10);
+        playerObj player2 = new playerObj("player2", 50, 10, 10);
+        matchmanager.newMatch(player1,player2);
+        matchmanager.cnnsvmSetting = false;
+        matchmanager.setFirstPlayer(false);
 
-        sendText.onClick.AddListener(judging);
-        
-        this.cnnsvmSetting = false;
-        this.theJudge = new aiRequester();
-        this.handleSql = new SQLliteHandler(sqlLoc, relativeLoc);
-        this.firstPlayer = "player1";
-
-        handleSql.CreateTable();
-        player1 = new playerObj("Player 1", 100, 20, 20);
-        player2 = new playerObj("Player 2", 100, 20, 20);
-
-        this.matchId = handleSql.newMatch();
-        player1.playerId = handleSql.newPlayer(player1);
-        player2.playerId = handleSql.newPlayer(player2);
-
-
-        if (firstPlayer == "player1")
-        {
-            this.currentPlayer = player1;
-        }
-        else
-        {
-            this.currentPlayer = player2;
-        }
+        sendText.onClick.AddListener(judgeEvent);
     }
 
-    private void judging()
+    public void judgeEvent()
     {
-        StartCoroutine(ArbitrateAndSwap());
+        matchmanager.judging(inputArea.text.Trim());
+        //outputArea.text = $"result: {matchmanager.result.ToString()}";
     }
 
-
-    private IEnumerator ArbitrateAndSwap()
+    private void OnEnable()
     {
-        string input = inputArea.text;
-        yield return StartCoroutine(arbitrate(input));
-
-        if (currentPlayer.playerName == "Player 1")
-        {
-            currentPlayer = player2;
-            Debug.Log($"Current Player: {currentPlayer.playerId}");
-        }
-        else
-        {
-            currentPlayer = player1;
-            Debug.Log($"Current Player: {currentPlayer.playerId}");
-        }
+        MatchManager.OnFinishTurn += invokeResult;
     }
 
-    private IEnumerator arbitrate(string input)
+    private void OnDisable()
     {
-        statementObj newstatementChatFilter = new statementObj(currentPlayer.playerId, matchId, input);
-
-        // Start the coroutine and wait for it to finish
-        yield return StartCoroutine(theJudge.SendJudgementPostRequest(newstatementChatFilter, handleJudgeMent));
+        MatchManager.OnFinishTurn -= invokeResult;
     }
 
-
-    public void displayJudgement(statementObj ratedStatement)
+    public void invokeResult(double result)
     {
-        outputArea.text = ratedStatement.statement;
-        outputArea.text = $"ratingCNNSVM: {ratedStatement.ratingCNNSVM} ratingChatFilter: {ratedStatement.ratingChatFilter}";
+        outputArea.text = result.ToString();
     }
 
-    private void handleJudgeMent(statementObj result)
-    {
-        handleSql.InsertStatement(result);
-        displayJudgement(result);
 
-        if (this.cnnsvmSetting)
-        {
-            if (result.ratingCNNSVM > 0.5)
-            {
-                handleAttack(result.ratingCNNSVM);
-            }
-
-            else
-            {
-                handleHeal(1 - result.ratingCNNSVM);
-            }
-        }
-        else
-        {
-            if (result.ratingChatFilter > 0.5)
-            {
-                handleAttack(result.ratingChatFilter);
-            }
-
-            else
-            {
-                handleHeal(1 - result.ratingChatFilter);
-            }
-
-        }
-    }
-
-    private void handleAttack(double percentDamage)
-    {
-        if (currentPlayer == player1)
-        {
-            player2.health -= (int)(player1.maxDamage * percentDamage);
-            currentPlayer = this.player2;
-        }
-        else
-        {
-            player1.health -= (int)(player2.maxDamage * percentDamage);
-            currentPlayer = this.player1;
-        }
-    }
-
-    private void handleHeal(double percentHeal)
-    {
-        if (currentPlayer == player1)
-        {
-            player2.health += (int)(player2.maxHeal * percentHeal);
-            if (player2.health > player2.maxHealth)
-            {
-                player2.health = player2.maxHealth;
-            }
-            currentPlayer = player1;
-        }
-        else
-        {
-            player1.health += (int)(player1.maxHeal * percentHeal);
-            if (player1.health > player1.maxHealth)
-            {
-                player1.health = player1.maxHealth;
-            }
-            currentPlayer = player2;
-        }
-    }
 }
