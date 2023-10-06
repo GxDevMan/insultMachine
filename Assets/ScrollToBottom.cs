ae6d5a9c9282c1710ba299403eaee3da08864984
Jez20 <Johneleazar.sandi.iics@ust.edu.ph>
2023-10-07 01:27:30 +0800

    Text Area
    Game Proper Scene Text Area Added Vertical Scroll Bar Functionalities

---
+++ Assets/ScrollToBottom.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollToBottom : MonoBehaviour
+{
+    public ScrollRect scrollRect;
+
+    public void ScrollToBottomOnClick()
+    {
+        // Set the verticalNormalizedPosition to 0 to scroll to the bottom.
+        scrollRect.verticalNormalizedPosition = 0f;
+    }
+}
